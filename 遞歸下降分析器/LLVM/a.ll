70787486b5d88f5997045448ad4f7bf3c9dcc8c0
Chen, Chien-ting <chenjt30@gmail.com>
2019-06-25 01:36:36 +0800

    add descent parser, llvm ir

---
+++ 遞歸下降分析器/LLVM/a.ll
@@ -0,0 +1,54 @@
+; ModuleID = 'a.cpp'
+target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-pc-linux-gnu"
+
+%"class.std::ios_base::Init" = type { i8 }
+
+@_ZStL8__ioinit = internal global %"class.std::ios_base::Init" zeroinitializer, align 1
+@__dso_handle = external global i8
+@llvm.global_ctors = appending global [1 x { i32, void ()*, i8* }] [{ i32, void ()*, i8* } { i32 65535, void ()* @_GLOBAL__sub_I_a.cpp, i8* null }]
+
+; Function Attrs: uwtable
+define internal void @__cxx_global_var_init() #0 section ".text.startup" {
+  call void @_ZNSt8ios_base4InitC1Ev(%"class.std::ios_base::Init"* @_ZStL8__ioinit)
+  %1 = call i32 @__cxa_atexit(void (i8*)* bitcast (void (%"class.std::ios_base::Init"*)* @_ZNSt8ios_base4InitD1Ev to void (i8*)*), i8* getelementptr inbounds (%"class.std::ios_base::Init", %"class.std::ios_base::Init"* @_ZStL8__ioinit, i32 0, i32 0), i8* @__dso_handle) #2
+  ret void
+}
+
+declare void @_ZNSt8ios_base4InitC1Ev(%"class.std::ios_base::Init"*) #1
+
+declare void @_ZNSt8ios_base4InitD1Ev(%"class.std::ios_base::Init"*) #1
+
+; Function Attrs: nounwind
+declare i32 @__cxa_atexit(void (i8*)*, i8*, i8*) #2
+
+; Function Attrs: norecurse nounwind uwtable
+define i32 @main() #3 {
+  %1 = alloca i32, align 4
+  %a = alloca i32, align 4
+  %b = alloca i32, align 4
+  store i32 0, i32* %1, align 4
+  store i32 12, i32* %a, align 4
+  %2 = load i32, i32* %a, align 4
+  %3 = load i32, i32* %a, align 4
+  %4 = mul nsw i32 %3, 3
+  %5 = add nsw i32 %2, %4
+  store i32 %5, i32* %b, align 4
+  %6 = load i32, i32* %b, align 4
+  ret i32 %6
+}
+
+; Function Attrs: uwtable
+define internal void @_GLOBAL__sub_I_a.cpp() #0 section ".text.startup" {
+  call void @__cxx_global_var_init()
+  ret void
+}
+
+attributes #0 = { uwtable "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-nans-fp-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+fxsr,+mmx,+sse,+sse2" "unsafe-fp-math"="false" "use-soft-float"="false" }
+attributes #1 = { "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-nans-fp-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+fxsr,+mmx,+sse,+sse2" "unsafe-fp-math"="false" "use-soft-float"="false" }
+attributes #2 = { nounwind }
+attributes #3 = { norecurse nounwind uwtable "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-nans-fp-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+fxsr,+mmx,+sse,+sse2" "unsafe-fp-math"="false" "use-soft-float"="false" }
+
+!llvm.ident = !{!0}
+
+!0 = !{!"clang version 3.8.0-2ubuntu4 (tags/RELEASE_380/final)"}
---
+++ 遞歸下降分析器/compiler_descent_parser.py
@@ -0,0 +1,154 @@
+import sys
+import re
+
+# command description
+cmd_desc = """
+simple compiler example
+compiler.py filename
+"""
+
+if len(sys.argv) == 1:
+    print(cmd_desc)
+if len(sys.argv) > 2:
+    raise Exception("you input too more arguments.")
+
+#input_filename = sys.argv[1]
+#file_content = open(input_filename,'r')
+
+# test code
+# test = "1 + 2 * (23 / 12)"
+test = "1 + 2 * 3 * 4"
+
+# TOKEN
+INT = r'(?P<INT>\d+)'
+
+ADD = r'(?P<ADD>\+)'
+SUB = r'(?P<SUB>-)'
+MUL = r'(?P<MUL>\*)'
+DIV = r'(?P<DIV>/)'
+LPARE = r'(?P<LPARE>\()'
+RPARE = r'(?P<RPARE>\))'
+ASSIGN = r'(?P<ASSIGN>=)'
+
+L_INT = r'(?P<L_INT>int)' # let int
+
+ID = r'(?P<ID>[_a-zA-Z][_0-9a-zA-Z]+)' #indicator
+WSNL = r'(?P<WS>(\s|\n|\r)+)'# whitespace & newline
+
+token_pattern_list = [INT, ADD, SUB, MUL, DIV , LPARE, RPARE, ASSIGN, L_INT , ID, WSNL]
+
+token_pattern = re.compile("|".join(token_pattern_list))
+
+raw_token_result_list = []
+
+for match in re.finditer(token_pattern, test):
+    raw_token_result_list.append((match.lastgroup, match.group(0)))
+
+# [(None, None)] 必加，以避免出錯
+token_result_list = [i for i in raw_token_result_list if i[0] != "WS"] + [(None, None)]
+print(token_result_list)
+
+
+"""
+expr = term {+ term }*
+term = factor {* factor}*
+factor = number
+"""
+class Parser:
+    def __init__(self):
+        self.token = None
+        self.remained_token = None
+    
+    def parse(self, token_list):
+        if len(token_list) == (None, None):
+            print("The compiled content is empty. Stop parsing.")
+            return None
+
+        self.token = token_list[0]
+        self.remained_token = token_list[1:]
+        result = self.expr()
+        print(result)
+
+        return result
+
+    def advance(self):
+        self.token = self.remained_token[0]
+        self.remained_token = self.remained_token[1:]
+
+    def expr(self):
+        result = self.term()
+        while self.token[0] == "ADD":
+            result_head = self.token[1]
+            self.advance()
+            rhs = self.term()
+            result = [result_head, result, rhs]
+            rhs = result
+        return result
+
+    
+    def term(self):
+        result = self.factor()
+        while self.token[0] == "MUL":
+            result_head = self.token[1]
+            self.advance()
+            rhs = self.factor()
+            result = [result_head, result, rhs]
+            rhs = result
+
+        return result
+     
+    def factor(self):
+        if self.token[0] != "INT":
+            raise Exception("Expect Number, find %s" % self.token[1])
+        else:
+            result = int(self.token[1])
+            self.advance()
+        
+        return result
+
+"""
+    def statements(self):
+        if token
+"""
+
+parser = Parser()
+parser.parse(token_result_list)
+
+'''
+日後的文法：
+
+statements ::= {statement}*
+statement ::= declare | exp
+declare ::= decl_lfs = exp
+decl_lfs ::= type var
+type ::= int # 記得加上或集 return False
+var ::= id
+exp ::= term {+/- term}*
+term ::= factor {*// factor}*
+factor ::= ( exp ) | const
+const ::= int
+
+    """或集時使用：expr = term1 (= +) | term2 (= int) """
+    def expr(self):
+        result = self.term1()
+        if result == False:
+            result = self.term2()
+            print("term2")
+        return result
+
+    def term1(self):
+        if self.token[0] == "ADD":
+            result = self.token[1]
+            self.advance()
+            return result
+        else:
+            return False # 或集時必加
+    
+    def term2(self):
+        if self.token[0] == "INT":
+            result = self.token[1]
+            self.advance()
+            return result
+        else:
+            raise Exception("錯誤")
+'''
