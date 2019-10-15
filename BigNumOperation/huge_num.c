69fcb7c5e4e4375697211db0e3ef121b56280e0d
Chen, Chien-ting <chenjt30@gmail.com>
2019-10-16 00:35:00 +0800

    add bignum quotient/remainder/printstr etc

--- BigNumOperation/huge_num.c
+++ BigNumOperation/huge_num.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+#include "huge_num.h"
 
 #define UNSIGNED_INT_MAX 4294967295
 
@@ -39,8 +41,8 @@ bool bigNumAbsGreaterThan(BigNum lhs, BigNum rhs){
       if (lhs.array[i] > rhs.array[i]){
         return true;
       }
-      return false;
     }
+    return false;
   }
 }
 
@@ -50,8 +52,8 @@ bool bigNumAbsEqual(BigNum lhs, BigNum rhs){
     if (lhs.array[i] != rhs.array[i]){
       return false;
     }
-  return true;
   }
+  return true;
 }
 
 bool bigNumEqual(BigNum lhs, BigNum rhs){
@@ -118,7 +120,7 @@ BigNum shiftRightOnce(BigNum num){
   }
 }
 
-BigNum Subtract(BigNum lhs, BigNum rhs){
+BigNum subtract(BigNum lhs, BigNum rhs){
   // lhs - 0 = lhs
   if (bigNumEqual(rhs, intToBigNum(0))){
     return lhs;
@@ -126,34 +128,34 @@ BigNum Subtract(BigNum lhs, BigNum rhs){
   // lhs - rhs = lhs + (- rhs) = lhs - new_rhs
   else{
     BigNum new_rhs = rhs;
-    new_rhs.isNotNeg = not(new_rhs.isNotNeg);
-    return Add(lhs, new_rhs);
+    new_rhs.isNotNeg = !(new_rhs.isNotNeg);
+    return add(lhs, new_rhs);
     }
   }
 
 // the real "add function"
-BigNum Add(BigNum lhs, BigNum rhs){
+BigNum add(BigNum lhs, BigNum rhs){
   if (lhs.isNotNeg == true && rhs.isNotNeg == true){
     BigNum result;
-    result = absAdd(lhs, rhs);
+    result = absadd(lhs, rhs);
     result.isNotNeg = true;
     return result;
   }
   else if(lhs.isNotNeg == false && rhs.isNotNeg == false){
     BigNum result;
-    result = absAdd(lhs, rhs);
+    result = absadd(lhs, rhs);
     result.isNotNeg = false;
     return result;
   }
   else if(lhs.isNotNeg == true && rhs.isNotNeg == false){
     BigNum result;
     if (bigNumAbsGreaterThan(lhs, rhs) && bigNumAbsEqual(lhs, rhs)){
-      result = absSubtract(lhs, rhs);
+      result = abssubstaction(lhs, rhs);
       result.isNotNeg = true;
       return result;
     }
     else{
-      result = absSubtract(rhs, lhs);
+      result = abssubstaction(rhs, lhs);
       result.isNotNeg = false;
       return result;
     }
@@ -161,12 +163,12 @@ BigNum Add(BigNum lhs, BigNum rhs){
   else{
     BigNum result;
     if (bigNumAbsGreaterThan(rhs, lhs) && bigNumAbsEqual(rhs, lhs)){
-      result = absSubtract(rhs, lhs);
+      result = abssubstaction(rhs, lhs);
       result.isNotNeg = true;
       return result;
     }
     else{
-      result = absSubtract(lhs, rhs);
+      result = abssubstaction(lhs, rhs);
       result.isNotNeg = false;
       return result;
     }
@@ -174,7 +176,7 @@ BigNum Add(BigNum lhs, BigNum rhs){
 }
 
 // |a| + |b|. a must be greater than b. 
-BigNum absAdd(BigNum a, BigNum b){
+BigNum absadd(BigNum a, BigNum b){
   BigNum result;
   if (a.length > b.length){
     result.length = a.length;
@@ -206,9 +208,11 @@ BigNum absAdd(BigNum a, BigNum b){
   return result;
 }
 
-BigNum absSubtract(BigNum a, BigNum b){
+BigNum abssubstaction(BigNum a, BigNum b){
   BigNum result;
   result.length = a.length;
+  result.array = malloc(sizeof(unsigned int)* result.length);
+
   for (int i=0; i < result.length; i++){
 
     unsigned int b_array_i;
@@ -222,26 +226,27 @@ BigNum absSubtract(BigNum a, BigNum b){
     if (a.array[i] < b_array_i){
       a.array[i+1] -= 1;
       
-      result.array[i] = a.array[i] - (UNSIGNED_INT_MAX - b_array_i);
+      result.array[i] =  a.array[i] + (UNSIGNED_INT_MAX - b_array_i);
     }
     else{
       result.array[i] = a.array[i] - b_array_i;
     }
+  }
 
-    for(int i=result.length-1; i >= 0; i--){
-      if (result.array[i] > 0){
-        break;
-      }
-      else{
-        if (i > 0){
-          result.array = realloc(result.array, result.length - 1);
-          result.length -= 1;
-        }
+  for(int i=result.length-1; i >= 0; i--){
+    if (result.array[i] > 0){
+      break;
+    }
+    else{
+      if (i > 0){
+        result.array = realloc(result.array, result.length - 1);
+        result.length -= 1;
       }
     }
+  }
 
     
-  }
+  
   
   return result;
 }
@@ -274,7 +279,6 @@ BigNum shiftLeftOnce(BigNum num){
 }
 
 //  << n
-
 BigNum shiftLeft(BigNum num, int n){
   for(int i=0;i<n;i++){
     num = shiftLeftOnce(num);
@@ -294,8 +298,7 @@ BigNum multiply(BigNum lhs, BigNum rhs){
   BigNum result;
 
   int temp_result_len = lhs.length + rhs.length;
-  unsigned int * result_abs = malloc(sizeof(int) * temp_result_len);
-  memset(result_abs, 0, temp_result_len);
+  unsigned int * result_abs = calloc(temp_result_len, sizeof(int));
 
   for (int i=0;i<lhs.length;i++){
     for (int j=0;j<rhs.length;j++){
@@ -317,8 +320,149 @@ BigNum multiply(BigNum lhs, BigNum rhs){
     result.isNotNeg = true;
   }
   else{
-    result.isNotNeg == false;
+    result.isNotNeg = false;
   }
+
+  return result;
+}
+
+/*
+find remainder (%).
+12 % 5 = 2 
+12 % -5 = 2
+-12 % 5 = -2
+-12 % -5 = -2
+*/
+BigNum BigNumRemainder(BigNum lhs, BigNum rhs){
+  BigNum bigNumZero = intToBigNum(0);
+
+  if (bigNumEqual(rhs,bigNumZero)){
+    printf("div 0 error, return 0");
+    return bigNumZero;
+  }
+  else{
+    BigNum q = quotient(lhs, rhs);
+    BigNum res = findRemainder(lhs, rhs, q);
+
+    return res;
+  }
+};
+
+BigNum findRemainder(BigNum lhs, BigNum rhs, BigNum quotient){
+  BigNum res;
+  res = subtract(lhs, multiply(rhs, quotient));
+  
+  return res;
+  }
+
+/* find quotient (//).
+12 // 5 = 2 
+12 // -5 = -2
+-12 // 5 = -2
+-12 // -5 = 2
+*/
+BigNum quotient(BigNum lhs, BigNum rhs){
+  BigNum bigNumZero = intToBigNum(0);
+
+  if (bigNumEqual(rhs,bigNumZero)){
+    printf("div 0 error, return 0");
+    return bigNumZero;
+  }
+  else if (bigNumAbsLessThan(lhs, rhs)){
+    return bigNumZero;
+  }
+  /* with Newton's Method, let's find the quotient.
+  see: http://yoxem.github.io/2019/10/12/tua7soo3ji7tu5huat4ian2sng3huat4/
+  */
+  else{
+    BigNum abs_lhs = lhs;
+    abs_lhs.isNotNeg = true;
+
+    BigNum abs_rhs = rhs;
+    abs_rhs.isNotNeg = true; // force abs_rhs be non-zero
+
+    BigNum y_k_minus_1 = intToBigNum(0); // y_(k-1)
+    BigNum y_k = intToBigNum(1); // y_k ; y_0 = 1
+
+    int p = lhs.length + 1;
+
+    while(! bigNumEqual(y_k, y_k_minus_1)){
+      y_k_minus_1 = y_k;
+
+      BigNum _2y_k = multiply(intToBigNum(2), y_k);
+      BigNum y_k_2_d = multiply(multiply(y_k, y_k), abs_rhs); // y_k^(2)*abs_rhs
+      BigNum y_k_2_d_leftshift_p = shiftLeft(y_k_2_d, p); // (y_k^2 * abs_rhs) << p
+
+      y_k = subtract(_2y_k, y_k_2_d_leftshift_p);
+
+
+    }
+
+    BigNum lhs_y_k = multiply(lhs, y_k); // lhs * y_k
+    BigNum quotient = shiftLeft(lhs_y_k,p); // lhs * y_k // << p
+
+
+    BigNum remainder = subtract(abs_lhs,multiply(abs_rhs,quotient));
+    // if |lhs| - q * |rhs| < 0 ; adjust value of q by decrease q
+    while (bigNumLessThan(remainder, bigNumZero)){
+      quotient = subtract(quotient, intToBigNum(1));
+      remainder = subtract(abs_lhs,multiply(abs_rhs,quotient));
+    }
+    
+    // if |lhs| - q * |rhs| > |rhs|; adjust value of q by increase q
+    while (bigNumGreaterThan(remainder, abs_rhs)){
+      quotient = add(quotient, intToBigNum(1));
+      remainder = subtract(abs_lhs, multiply(abs_rhs, quotient));
+    }
+
+    if(lhs.isNotNeg != rhs.isNotNeg){
+      quotient.isNotNeg = false;
+    }
+
+    return quotient;
+  }
+}
+
+
+/* 
+convert to decminal number string.
+*/
+char* BigNumToDecStr(BigNum n){
+  char* result;
+
+
+  bool n_sign = n.isNotNeg;
+  n.isNotNeg = true; // set it to be non-zero
+  
+  BigNum bigNumZero = intToBigNum(0);
+  BigNum bigNum10 = intToBigNum(10);
+
+  while(!bigNumEqual(n, bigNumZero)){
+    char* digitStr;
+    BigNum quotient_of_div_10 = quotient(n,bigNum10);
+    BigNum digitBigNum =  findRemainder(n, bigNum10, quotient_of_div_10);
+
+    unsigned int digit = digitBigNum.array[0];
+    sprintf(digitStr, "%u", digit);
+
+    char* new_result = malloc(sizeof(char)*(strlen(result)+1)); // strlen(digitStr) == 1
+    strcat(new_result, result);
+    strcat(new_result, digitStr);
+    
+    result = new_result;
+
+    n = quotient_of_div_10;
+    }
+
+  // if it is neg. num, add a minus "-" sign.
+  if (!n_sign){
+    char* result_sign = "-";
+    char* new_result = malloc(sizeof(char)*(strlen(result)+1));
+    strcat(new_result, result_sign);
+    strcat(new_result, result);
+
+    result = new_result;
+  };
 
   return result;
 }
@@ -330,9 +474,15 @@ int main(void){
     g = shiftLeft(g, 3);
     g = shiftRight(g, 3);
 
-    bool c = bigNumGreaterThan(a, b);
-    bool d = bigNumLessThan(a, b);
-    bool e = bigNumEqual(a, b);
+    BigNum x = quotient(intToBigNum(76),intToBigNum(4));
+
+    printf("%s", BigNumToDecStr(a));
+    printf("%s", BigNumToDecStr(b));
+    printf("%s", BigNumToDecStr(g));
+
+    // bool c = bigNumGreaterThan(a, b);
+    // bool d = bigNumLessThan(a, b);
+    // bool e = bigNumEqual(a, b);
     
     // BigNum l = get_length_n_zero_array(10 >)
     return 0;
---
+++ BigNumOperation/huge_num.h
@@ -0,0 +1,26 @@
+#ifndef HUGE_NUM
+#define HUGE_NUM
+
+typedef struct bigNum BigNum;
+
+BigNum add(BigNum lhs, BigNum rhs);
+BigNum absadd(BigNum a, BigNum b);
+BigNum abssubstaction(BigNum a, BigNum b);
+bool bigNumAbsEqual(BigNum lhs, BigNum rhs);
+bool bigNumAbsGreaterThan(BigNum lhs, BigNum rhs);
+bool bigNumAbsLessThan(BigNum lhs, BigNum rhs);
+bool bigNumEqual(BigNum lhs, BigNum rhs);
+bool bigNumGreaterThan(BigNum lhs, BigNum rhs);
+bool bigNumLessThan(BigNum lhs, BigNum rhs);
+BigNum intToBigNum(int i);
+int main(void);
+BigNum multiply(BigNum lhs, BigNum rhs);
+BigNum quotient(BigNum lhs, BigNum rhs);
+BigNum shiftLeft(BigNum num, int n);
+BigNum shiftLeftOnce(BigNum num);
+BigNum shiftRight(BigNum num, int n);
+BigNum shiftRightOnce(BigNum num);
+BigNum subtract(BigNum lhs, BigNum rhs);
+BigNum BigNumRemainder(BigNum lhs, BigNum rhs);
+BigNum findRemainder(BigNum lhs, BigNum rhs, BigNum quotient);
+#endif
